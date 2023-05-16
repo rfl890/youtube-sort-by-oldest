a1dc1017d21fdcafce94830b042702fc81ffdd66
rfl890 <87506407+rfl890@users.noreply.github.com>
2023-05-16 16:33:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const getPlaylistIdFromYoutubeChannelUrl = async (url) => {
     const data = await (await fetch("https://uncors.vercel.app/?" + new URLSearchParams({
         url: url
     }))).text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(data, "text/html");
-    if (doc.querySelector('meta[itemProp="channelId"]')) {
-        return "UU" + doc.querySelector('meta[itemProp="channelId"]').content.slice(2);
-    }
+    const result = data.match(/"(channelId)":"((\\"|[^"])*)"/i);
+    return result ? (typeof result[2] === "string" && result[2].replace("UC", "UU")) : null; 
 }
 
 const youtubeUrlInput = $("#youtube_url");
